a237db3225f321abdc964f7098bd408a2ada5ea6
Your Name <you@example.com>
2020-08-19 00:08:20 +0530

    first commit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,8 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url="http://api.weatherstack.com/current?access_key=17a4892219672010337f5190adca3526&query="+latitude+","+longitude;
+
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions)
         }
     })
 }
